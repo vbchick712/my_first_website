1b50a3854aae9ac1cf78eda4ee59aaae61cddba8
Shannan Muench <shannan.muench@gmail.com>
2017-05-09 14:27:56 -0400

    first heroku commit

---
+++ server.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+get '/' do
+  "Helloooooooooo WORLD!!!!!!!!!"
+end
+
+get "/sinatra" do
+  "Hellooooooooo SINATRA!!!!!!!"
+end
+
+get '/woohoo' do
+  "Woooohoooooooooooo!!!!!!!!!!"
+end
